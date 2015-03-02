716ddf5f1ac7fd953bbaa7b6d6ad57ec5185c318
AlessandroDelSole <alessandro.delsole@visual-basic.it>
2015-03-02 23:52:09 +0100

    Added DateTimeOffset analyzer to VB for Windows apps

---
+++ DNA.VisualBasic/DNA.VisualBasic/Windows/DateTimeOffsetAnalyzer.vb
@@ -0,0 +1,62 @@
+﻿Imports System.Collections.Immutable
+Imports System.Threading
+Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.Diagnostics
+Imports Microsoft.CodeAnalysis.Text
+Imports Microsoft.CodeAnalysis.VisualBasic
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+
+<DiagnosticAnalyzer(LanguageNames.VisualBasic)>
+Public Class DateTimeOffsetAnalyzer
+    Inherits DiagnosticAnalyzer
+
+    Public Const DiagnosticId = "DNA002"
+    Friend Shared ReadOnly Title As String = "Declaration of type System.DateTime"
+    Friend Shared ReadOnly MessageFormat As String = "Platform warning: '{0}'"
+    Friend Shared ReadOnly Description As String = "Checks for improper usage of System.DateTime in Windows Runtime"
+    Friend Const Category = "Naming"
+
+    Friend Shared Rule As New DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, True)
+
+    Public Overrides ReadOnly Property SupportedDiagnostics As ImmutableArray(Of DiagnosticDescriptor)
+        Get
+            Return ImmutableArray.Create(Rule)
+        End Get
+    End Property
+
+    Public Overrides Sub Initialize(context As AnalysisContext)
+        context.RegisterSyntaxNodeAction(AddressOf AnalyzeDate, SyntaxKind.PredefinedType)
+        context.RegisterSyntaxNodeAction(AddressOf AnalyzeDate, SyntaxKind.IdentifierName)
+    End Sub
+
+    Private Sub AnalyzeDate(context As SyntaxNodeAnalysisContext)
+        If context.SemanticModel.Compilation.GetTypeByMetadataName("Windows.Storage.StorageFile") Is Nothing Then
+            Return
+        End If
+
+        Dim root = context.Node
+
+        If TypeOf (root) Is PredefinedTypeSyntax Then
+            root = CType(context.Node, PredefinedTypeSyntax)
+        ElseIf TypeOf (root) Is IdentifierNameSyntax
+            root = CType(context.Node, IdentifierNameSyntax)
+        Else
+            Return
+        End If
+
+        Dim dateSymbol = TryCast(context.SemanticModel.GetSymbolInfo(root).Symbol, INamedTypeSymbol)
+
+        If dateSymbol Is Nothing Then
+            Return
+        End If
+
+        If Not dateSymbol.MetadataName = "DateTime" Then
+            Return
+        End If
+
+        Dim diagn = Diagnostic.Create(Rule, root.GetLocation,
+                              "Consider replacing with DateTimeOffset")
+        context.ReportDiagnostic(diagn)
+
+    End Sub
+End Class
---
+++ DNA.VisualBasic/DNA.VisualBasic/Windows/DateTimeOffsetCodeFix.vb
@@ -0,0 +1,79 @@
+﻿Imports System.Collections.Immutable
+Imports System.Threading
+Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.CodeActions
+Imports Microsoft.CodeAnalysis.CodeFixes
+Imports Microsoft.CodeAnalysis.CodeRefactorings
+Imports Microsoft.CodeAnalysis.Editing
+Imports Microsoft.CodeAnalysis.Rename
+Imports Microsoft.CodeAnalysis.Text
+Imports Microsoft.CodeAnalysis.VisualBasic
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+
+<ExportCodeFixProvider(LanguageNames.VisualBasic, Name:=NameOf(DateTimeOffsetCodeFix))>
+Public Class DateTimeOffsetCodeFix
+    Inherits CodeFixProvider
+
+    ' TODO: Replace with actual diagnostic id that should trigger this fix.
+    Public Const DiagnosticId As String = NameOf(DateTimeOffsetAnalyzer.DiagnosticId)
+
+    Public NotOverridable Overrides ReadOnly Property FixableDiagnosticIds As ImmutableArray(Of String)
+        Get
+            Return ImmutableArray.Create(DiagnosticId)
+        End Get
+    End Property
+
+    Public NotOverridable Overrides Function GetFixAllProvider() As FixAllProvider
+        Return WellKnownFixAllProviders.BatchFixer
+    End Function
+
+    Public NotOverridable Overrides Async Function RegisterCodeFixesAsync(context As CodeFixContext) As Task
+        Dim root = Await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(False)
+
+        ' TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+
+        Dim diagnostic = context.Diagnostics.First()
+        Dim diagnosticSpan = diagnostic.Location.SourceSpan
+
+        Dim node = root.FindNode(context.Span)
+
+        If TypeOf (node) Is PredefinedTypeSyntax Then
+            context.RegisterCodeFix(
+            CodeAction.Create("Replace with DateTimeOffset",
+                              Function(c) ReplaceDateAsync(context.Document, node, c)), diagnostic)
+        Else
+            context.RegisterCodeFix(
+            CodeAction.Create("Replace with DateTimeOffset",
+                              Function(c) ReplaceDateTimeAsync(context.Document, node, c)), diagnostic)
+        End If
+    End Function
+
+    Private Async Function ReplaceDateAsync(document As Document, node As PredefinedTypeSyntax,
+                                     cancellationToken As CancellationToken) As Task(Of Document)
+
+        'Dim semanticModel = Await document.GetSemanticModelAsync(cancellationToken)
+        Dim root = Await document.GetSyntaxRootAsync
+        Dim generator = SyntaxGenerator.GetGenerator(document)
+
+        Dim newIdentifierSyntax = generator.IdentifierName("DateTimeOffset").WithTrailingTrivia(node.GetTrailingTrivia)
+
+        Dim newRoot = root.ReplaceNode(node, newIdentifierSyntax)
+        Dim newDocument = document.WithSyntaxRoot(newRoot)
+        Return newDocument
+    End Function
+
+    Private Async Function ReplaceDateTimeAsync(document As Document, node As IdentifierNameSyntax,
+                                     cancellationToken As CancellationToken) As Task(Of Document)
+
+        Dim semanticModel = Await document.GetSemanticModelAsync(cancellationToken)
+        Dim root = Await document.GetSyntaxRootAsync
+
+        'DateTime
+        Dim node2 = node.WithIdentifier(SyntaxFactory.ParseToken("DateTimeOffset")).WithTrailingTrivia(node.GetTrailingTrivia)
+        Dim newRoot = root.ReplaceNode(node, node2)
+
+        Dim newDocument = document.WithSyntaxRoot(newRoot)
+        Return newDocument
+
+    End Function
+End Class

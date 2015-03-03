ec706c2c2fb4f60f1e4e536bc0bdf94e5e100caf
Alessandro Del Sole <alessandro.delsole@visual-basic.it>
2015-03-03 19:21:01 +0100

    Added analyzer and code fix for missing FromODataUri attributes in OData methods (VB Only)

---
+++ DNA.VisualBasic/DNA.VisualBasic/OData/ODataCheckFromODataUriAnalyzer.vb
@@ -0,0 +1,84 @@
+﻿Imports System.Collections.Immutable
+Imports System.Threading
+Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.Diagnostics
+Imports Microsoft.CodeAnalysis.Text
+Imports Microsoft.CodeAnalysis.VisualBasic
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+
+<DiagnosticAnalyzer(LanguageNames.VisualBasic)>
+Public Class ODataCheckFromODataUriAnalyzer
+    Inherits DiagnosticAnalyzer
+
+    Public Const DiagnosticId = "DNA102"
+    Friend Shared ReadOnly Title As String = "OData warning"
+    Friend Shared ReadOnly MessageFormat As String = "'{0}' is the first parameter and should be decorated with <FromODataUri>"
+    Friend Const Category = "Syntax"
+
+    Friend Shared Rule As New DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, True)
+
+    Public Overrides ReadOnly Property SupportedDiagnostics As ImmutableArray(Of DiagnosticDescriptor)
+        Get
+            Return ImmutableArray.Create(Rule)
+        End Get
+    End Property
+
+    Public Overrides Sub Initialize(context As AnalysisContext)
+        context.RegisterSyntaxNodeAction(AddressOf AnalyzeFromODataUri, SyntaxKind.FunctionStatement
+                                         )
+    End Sub
+
+    Private Sub AnalyzeFromODataUri(context As SyntaxNodeAnalysisContext)
+        If context.SemanticModel.Compilation.GetTypeByMetadataName("Microsoft.OData.Core.ODataAction") Is Nothing Then
+            Return
+        End If
+
+        Dim root = context.Node
+        Dim method As MethodStatementSyntax
+        If TypeOf (root) Is MethodStatementSyntax Then
+            method = TryCast(context.Node, MethodStatementSyntax)
+        Else
+            Return
+        End If
+
+        Dim supportedMethods As New List(Of String) From {"Get", "Post", "Put", "Delete", "Patch", "[Get]"}
+
+        If supportedMethods.Contains(method.Identifier.Value.ToString) Then
+
+            'Has parameters?
+            Dim firstParameterList = root.DescendantNodes.OfType(Of ParameterListSyntax).FirstOrDefault
+            If firstParameterList Is Nothing Then
+                Return
+            End If
+
+            Dim firstParameter = firstParameterList.Parameters.FirstOrDefault
+            If firstParameter Is Nothing Then
+                Return
+            End If
+
+            If firstParameter.AttributeLists = Nothing Then
+                'No attributes, so must add one
+                Dim diagn = Diagnostic.Create(Rule, firstParameter.GetLocation,
+                method.ParameterList.Parameters.FirstOrDefault.Identifier.ToString)
+                context.ReportDiagnostic(diagn)
+            Else
+                'For each attrib
+                For Each attrib In firstParameter.AttributeLists
+                    'If at least one contains FromODataUri, return
+                    If attrib.Attributes.Any(Function(a) a.Name.ToString.ToLowerInvariant.Contains("fromodatauri")) Then
+                        Return
+                    End If
+                Next
+                'If not, create a diagnostic
+                Dim diagn = Diagnostic.Create(Rule, firstParameter.GetLocation,
+                    method.ParameterList.Parameters.FirstOrDefault.Identifier.ToString)
+                context.ReportDiagnostic(diagn)
+            End If
+
+        Else
+            Return
+        End If
+    End Sub
+
+
+End Class
---
+++ DNA.VisualBasic/DNA.VisualBasic/OData/ODataCheckFromODataUriCodeFix.vb
@@ -0,0 +1,58 @@
+﻿Imports System.Collections.Immutable
+Imports System.Threading
+Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.CodeActions
+Imports Microsoft.CodeAnalysis.CodeFixes
+Imports Microsoft.CodeAnalysis.CodeRefactorings
+Imports Microsoft.CodeAnalysis.Rename
+Imports Microsoft.CodeAnalysis.Text
+Imports Microsoft.CodeAnalysis.VisualBasic
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+
+<ExportCodeFixProvider(LanguageNames.VisualBasic, Name:=NameOf(ODataCheckFromODataUriCodeFix))>
+Public Class ODataCheckFromODataUriCodeFix
+    Inherits CodeFixProvider
+
+    ' TODO: Replace with actual diagnostic id that should trigger this fix.
+    Public Const DiagnosticId As String = "DNA102"
+
+    Public NotOverridable Overrides ReadOnly Property FixableDiagnosticIds As ImmutableArray(Of String)
+        Get
+            Return ImmutableArray.Create(DiagnosticId)
+        End Get
+    End Property
+
+    Public NotOverridable Overrides Function GetFixAllProvider() As FixAllProvider
+        Return WellKnownFixAllProviders.BatchFixer
+    End Function
+
+    Public NotOverridable Overrides Async Function RegisterCodeFixesAsync(context As CodeFixContext) As Task
+        Dim root = Await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(False)
+
+        ' TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+
+        Dim diagnostic = context.Diagnostics.First()
+        Dim diagnosticSpan = diagnostic.Location.SourceSpan
+
+        Dim node = TryCast(root.FindNode(context.Span), ParameterSyntax)
+
+        context.RegisterCodeFix(
+        CodeAction.Create("Add <FromODataUri>",
+                          Function(c) ApplyFromODataUriAsync(context.Document, node, c)), diagnostic)
+    End Function
+
+    Private Async Function ApplyFromODataUriAsync(document As Document, node As ParameterSyntax,
+                             cancellationToken As CancellationToken) As Task(Of Document)
+
+        Dim root = Await document.GetSyntaxRootAsync
+
+        Dim newParameter = node.WithAttributeLists(node.AttributeLists.Add(SyntaxFactory.AttributeList(
+                    SyntaxFactory.SingletonSeparatedList(Of AttributeSyntax)(
+                        SyntaxFactory.Attribute(
+                            SyntaxFactory.IdentifierName("FromODataUri"))))))
+
+        Dim newRoot = root.ReplaceNode(node, newParameter)
+        Dim newDocument = document.WithSyntaxRoot(newRoot)
+        Return newDocument
+    End Function
+End Class
